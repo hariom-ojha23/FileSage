551f0f0758aa18ecf9c84180063f3f11be16d414
hariom-ojha23 <hariomojha2301@gmail.com>
2023-10-15 16:29:55 +0530

    displaying messages in chat box

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -3,6 +3,7 @@ import { privateProcedure, publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { db } from "@/db";
 import { z } from "zod";
+import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query";
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
@@ -35,17 +36,51 @@ export const appRouter = router({
       where: { userId },
     });
   }),
+  getFileMessages: privateProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100).nullish(),
+        cursor: z.string().nullish(),
+        fileId: z.string(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { userId } = ctx;
+      const { fileId, cursor } = input;
+      const limit = input.limit ?? INFINITE_QUERY_LIMIT;
+
+      const file = db.file.findFirst({
+        where: { id: fileId, userId },
+      });
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
+
+      const messages = await db.message.findMany({
+        where: { fileId },
+        take: limit + 1,
+        orderBy: { createdAt: "desc" },
+        cursor: cursor ? { id: cursor } : undefined,
+        select: { id: true, isUserMessage: true, createdAt: true, text: true },
+      });
+
+      let nextCursor: typeof cursor | undefined = undefined
+      if (messages.length > limit) {
+        const nextItem = messages.pop()
+        nextCursor = nextItem?.id
+      }
+
+      return  { messages, nextCursor }
+    }),
   getFileUploadStatus: privateProcedure
     .input(z.object({ fileId: z.string() }))
     .query(async ({ ctx, input }) => {
-      const { userId } = ctx
+      const { userId } = ctx;
 
       const file = await db.file.findFirst({
-        where: { id: input.fileId, userId }
-      })
-      if (!file) return { status: 'PENDING' as const }
+        where: { id: input.fileId, userId },
+      });
+      if (!file) return { status: "PENDING" as const };
 
-      return { status: file.uploadStatus }
+      return { status: file.uploadStatus };
     }),
   getFile: privateProcedure
     .input(z.object({ key: z.string() }))
